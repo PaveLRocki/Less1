f286e59a1ffe0de449099e78056070fede620dae
PaveLRocki <kasperscki@yandex.ru>
2023-01-21 23:21:01 +0300

    123

---
+++ src/Lesson1/Person.java
@@ -0,0 +1,36 @@
+package Lesson1;
+
+public class Person {
+    public static final String APPLICATION_ADREES = "Ледовый дворец";
+    private int age;
+    private String name;
+    private double salary = 1000;
+    public static void run (){
+
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
---
+++ src/Lesson1/Runner.java
@@ -0,0 +1,13 @@
+package Lesson1;
+
+public class Runner {
+
+
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(Person.APPLICATION_ADREES);
+
+    }
+
+}
